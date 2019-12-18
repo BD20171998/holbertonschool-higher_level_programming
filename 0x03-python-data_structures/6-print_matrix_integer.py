927236dfcf795103fb0bb68e5abe9eafcfc9d3df
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-18 04:57:35 +0000

    6. Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+
+    for i in range(0, len(matrix)):
+        x = len(matrix[i])
+
+        for j in range(0, x):
+            if j == x - 1:
+                print('{}'.format(matrix[i][j]))
+                break
+            print('{} '.format(matrix[i][j]), end="")
