ba4579c901d6c9c2c9d1a97fd39239b665f7ea79
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-18 07:44:45 +0000

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+
+    if not my_list:
+        return None
+
+    j = my_list[0]
+    for i in range(0, len(my_list)):
+        if j < my_list[i]:
+            j = my_list[i]
+    return j
