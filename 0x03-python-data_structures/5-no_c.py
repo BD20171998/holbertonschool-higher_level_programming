77ad38345f843894f0c3d84bcada985b989ae62a
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-18 01:51:35 +0000

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
 
     if my_string:
         lst = my_string[:]
         a = ["c", "C"]
-        lst = "".join(i for i in lst if i not in a)
-        return lst
+        st = "".join(i for i in lst if i not in a)
+        return st
