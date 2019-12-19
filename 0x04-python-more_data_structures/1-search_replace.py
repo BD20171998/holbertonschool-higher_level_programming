1538f7ca727268399fc349f5cfc528db70379ef0
Bob Deprizio <robert.deprizio@gmail.com>
2019-12-19 23:54:19 +0000

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+
+    newlist = my_list[:]
+
+    for i in newlist:
+        if i == search:
+            newlist[i] = replace
+
+    return newlist
