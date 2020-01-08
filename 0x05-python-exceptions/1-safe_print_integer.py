f73548749a9eecb24be1bbfe802ae7f5480dfb78
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-08 23:07:43 +0000

    1. Safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ def safe_print_integer(value):
     try:
         if isinstance(value, int) is True:
             print('{:d}'.format(value))
-            return (True)
+            return True
 
     except:
-        return (False)
+        return False
