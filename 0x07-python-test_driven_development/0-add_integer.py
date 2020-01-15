5fa971a1eca6e98d195db5ecd94508e2fd56c3b1
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-15 05:27:27 +0000

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,10 +13,12 @@ def add_integer(a, b=98):
     This function adds 2 integers and returns the sum
     """
 
-    if type(a) is not float and type(a) is not int:
+    if ((type(a) is not float and type(a) is not int) or a != a or
+            a == float('inf') or a == -float('inf')):
         raise TypeError('a must be an integer')
 
-    if type(b) is not float and type(b) is not int:
+    if ((type(b) is not float and type(b) is not int) or b != b or
+            b == float('inf') or b == -float('inf')):
         raise TypeError('b must be an integer')
 
     x = int(a)
