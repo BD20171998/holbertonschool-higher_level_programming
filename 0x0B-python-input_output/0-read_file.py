42290fa7f948b7387f0ce6fb0e4312079fa0390e
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-21 21:43:42 +0000

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""
+>>> read_file = __import__('0-read_file').read_file
+>>> read_file("my_file.txt")
+This is the only line from a file
+"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, encoding='utf-8', mode='r') as f:
+        print(f.read())
+        f.close()
