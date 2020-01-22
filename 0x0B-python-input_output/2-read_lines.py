650d15d1096cb311975f6deef63c078528a735a1
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-22 01:32:49 +0000

    2. Read n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+"""This is an example of the read_lines function
+
+>>> read_lines = __import__('2-read_lines').read_lines
+>>> print("1 line:")
+>>> read_lines("my_file_0.txt", 1)
+Holberton School offers a truly innovative approach to education:
+
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    This function reads n lines of a text file (UTF8) and prints
+    it to stdout. Reads entire file for nb_lines <= 0
+    """
+    with open(filename, encoding='utf-8', mode='r') as f:
+            x = 0
+            lines = f.readlines()
+            last = lines[-1]
+
+            if nb_lines <= 0:
+                for i in lines:
+                    if i is last:
+                        print(i, end='')
+                        f.close()
+                        return
+                    print(i)
+
+            else:
+                for line in lines:
+
+                    if x == nb_lines - 1 or line is last:
+                        print(line, end='')
+                        f.close()
+                        return
+
+                    print(line)
+                    x += 1
