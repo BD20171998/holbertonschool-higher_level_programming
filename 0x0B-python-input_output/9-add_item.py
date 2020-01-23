6df578f790ea99da169bc1b63615218328feb259
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-23 02:28:51 +0000

    9. Load, add, save

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+
+"""
+Imports the json and sys modules
+"""
 import json
 from sys import argv
+"""
+Imports modules for custom functions to be used
+"""
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
@@ -12,6 +19,8 @@ try:
     mylist = load_from_json_file("add_item.json")
     for i in range(1, len(argv)):
         mylist.append(argv[i])
+    save_to_json_file(mylist, "add_item.json")
+
 except:
 
     mylist = []
