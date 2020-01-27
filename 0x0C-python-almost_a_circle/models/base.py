0dad5e2156680b3f81ce8f84a07858f293f9dfa7
Bob Deprizio <robert.deprizio@gmail.com>
2020-01-27 22:35:23 +0000

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,9 +29,9 @@ class Base:
         a list of dictionaries
         """
 
-        jlist = []
+        jlist = '[]'
         if len(list_dictionaries) == 0 or list_dictionaries is None:
-            return dumps(jlist)
+            return jlist
 
         return dumps(list_dictionaries)
 
@@ -44,9 +44,10 @@ class Base:
         rs_json = []
         rs_json2 = []
 
-        if list_objs is None:
+        if list_objs is None or len(list_objs) == 0:
             with open("empty.json", mode='a', encoding='utf-8') as g:
                 dump(rs_json, g)
+                g.close()
 
         classname = cls.__name__
 
@@ -103,12 +104,14 @@ class Base:
                 rs_json2.append(obj_dict2)
 
         if classname == "Rectangle":
-            with open(classname+".json", mode='w', encoding='utf-8') as f:
-                dump(rs_json2, f)
+            with open(classname+".json", mode='w', encoding='utf-8') as f1:
+                dump(rs_json2, f1)
+                f1.close()
 
         if classname == "Square":
-            with open(classname+".json", mode='w', encoding='utf-8') as f:
-                dump(rs_json, f)
+            with open(classname+".json", mode='w', encoding='utf-8') as f2:
+                dump(rs_json, f2)
+                f2.close()
 
     @staticmethod
     def from_json_string(json_string):
