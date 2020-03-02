0c024c61a6f600693e0d3fb1368017f3ff0fef87
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-02 23:00:41 +0000

    Added additional flag for script

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database
 -- hbtn_0c_0 in a MySQL server
-SELECT score, name from second_table ORDER BY score;
+SELECT score, name from second_table ORDER BY score --desc;
