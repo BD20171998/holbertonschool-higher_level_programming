3176435aa9591a55f805bc51058687ce03c4cbb8
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-02 23:54:31 +0000

    Updated query using GROUP BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- script that lists the number of records with the same score in the table
 -- second_table of the database hbtn_0c_0 in a MySQL server
-SELECT score, COUNT(score) AS number FROM second_table ORDER BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score HAVING
+COUNT(score) >= 1 ORDER BY score DESC;
