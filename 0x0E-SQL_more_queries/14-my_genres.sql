ed12138f61edd671111b008ce551d809ca20da84
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-04 05:20:28 +0000

    Fixed syntax error

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the
 -- show Dexter
-SELECT tv_genres.name FROM tv_genres WHERE tv_genres.name = 'Dexter'
+SELECT tv_genres.name FROM tv_genres WHERE tv_shows.name = 'Dexter'
 AND tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_genres.name ASC;
