238c6da3c970308568cfc55a91ecc51dcd705b5e
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-04 03:43:00 +0000

    Added ASC for sort

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_genres, tv_shows, tv_show_genres
 WHERE tv_genres.id = tv_show_genres.genre_id
 AND tv_shows.id = tv_show_genres.show.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
