68804ba358896c15030aedcbb37c4476ed8385b0
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-04 01:11:28 +0000

    Added data type for ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- script that creates the table unique_id on a MySQL server
-CREATE TABLE IF NOT EXISTS unique_id(id UNIQUE DEFAULT 1,
+CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1,
 name VARCHAR(256));
