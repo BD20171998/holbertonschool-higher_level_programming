1c2a54f7c144e4477363b1bbb5651edb4513b12d
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-04 01:04:46 +0000

    Using UNIQUE statement instead

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- script that creates the table unique_id on a MySQL server
-CREATE TABLE IF NOT EXISTS unique_id(id NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS unique_id(id UNIQUE DEFAULT 1,
 name VARCHAR(256);
