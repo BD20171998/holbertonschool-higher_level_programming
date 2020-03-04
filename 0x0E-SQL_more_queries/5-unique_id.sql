aabe4d4163ace5938d6dd27e71ee9d79d3a85b9c
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-04 00:57:24 +0000

    Deleted default value for id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,2 @@
 -- script that creates the table unique_id on a MySQL server
-CREATE TABLE IF NOT EXISTS unique_id(id AUTO_INCREMENT DEFAULT 1,
-name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS unique_id(id AUTO_INCREMENT, name VARCHAR(256);
