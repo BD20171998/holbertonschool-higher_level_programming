b657952a120e034ab4e2367d1ef811753d09cb4b
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-04 01:02:04 +0000

    Added NOT NULL for id field

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,3 @@
 -- script that creates the table unique_id on a MySQL server
-CREATE TABLE IF NOT EXISTS unique_id(id AUTO_INCREMENT, name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS unique_id(id NOT NULL AUTO_INCREMENT,
+name VARCHAR(256);
