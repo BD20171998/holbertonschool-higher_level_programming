53223403d605021810227fe2b0504cfc19b722a5
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-14 19:58:07 +0000

    Changed script for table and field update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,11 @@
 -- script that converts hbtn_0c_0 database to UTF8 in a MySQL server
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
       CONVERT TO CHARACTER SET utf8mb4
       COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-      CHANGE first_name
+ALTER TABLE hbtn_0c_0.first_table
+      MODIFY hbtn_0c_0.first_table.name varchar(256)
       CHARACTER SET utf8mb4
       COLLATE utf8mb4_unicode_ci;
