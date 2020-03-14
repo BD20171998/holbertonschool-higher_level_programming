1dedcbfca0a2a566d4ffb4db1954ed2f2fe23ab5
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-14 20:12:45 +0000

    Adding MySQL script

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- script that displays the average temperature (Fahrenheit) by city ordered by
+-- temperature (descending)
+SELECT city, AVERAGE(value) as avg_temp
+FROM temperatures
+ORDER BY avg_temp DESC;
