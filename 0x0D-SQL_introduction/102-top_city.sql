d8978d9278dd6bcf5621112dbf0abfce8af0e010
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-14 20:39:43 +0000

    Changed month values for WHERE statement

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- ordered by temperature (descending)
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE month = 3 OR month = 4
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
