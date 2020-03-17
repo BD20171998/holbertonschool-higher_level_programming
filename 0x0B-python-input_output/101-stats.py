37b72e2e174f2b671d6a475ad142d9e432adfab0
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-17 03:29:36 +0000

    Rewrote how script receives input

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,25 +8,25 @@ stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0,
 count = 0
 filetotal = 0
 
-try:
+with open(sys.srgv, encoding='utf-8', mode='r') as f:
+    try:
+        for line in f:
+            a = line.split()
+            filetotal += int(a[8])
 
-    for line in sys.stdin:
-        a = line.split()
-        filetotal += int(a[8])
+            if count == 10:
+                print("File size: {}".format(filetotal))
+                for k, v in sorted(stats.items()):
+                    if v > 0:
+                        print("{}: {}".format(k, v))
+                count = 0
 
-        if count == 10:
-            print("File size: {}".format(filetotal))
-            for k, v in sorted(stats.items()):
-                if v > 0:
-                    print("{}: {}".format(k, v))
-            count = 0
+            if a[7] in stats.keys():
+                stats[a[7]] += 1
+                count += 1
 
-        if a[7] in stats.keys():
-            stats[a[7]] += 1
-            count += 1
-
-except KeyboardInterrupt:
-    print("File size: {}".format(filetotal))
-    for k, v in sorted(stats.items()):
-        if v > 0:
-            print("{}: {}".format(k, v))
+    except KeyboardInterrupt:
+        print("File size: {}".format(filetotal))
+        for k, v in sorted(stats.items()):
+            if v > 0:
+                print("{}: {}".format(k, v))
