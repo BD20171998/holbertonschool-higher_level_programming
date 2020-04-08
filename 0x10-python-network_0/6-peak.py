a49ddee509abe0f1d81d2f71981aac2869e6e21e
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-08 06:30:22 +0000

    Adding new algo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,21 @@
 
 
 def find_peak(list_of_integers):
+
     if list_of_integers == [] or list_of_integers is None:
         return None
 
-    temp = list_of_integers[:]
-    temp.sort(reverse=True)
-    return temp[0]
+    def binarySearch(arr, l, r):
+        if l == r:
+            return l
+
+        mid = (r + l) / 2
+
+        mid = int(mid)
+
+        if arr[mid] > arr[mid + 1]:
+            return binarySearch(arr, l, mid)
+
+        return binarySearch(arr, mid + 1, r)
+
+    return binarySearch(list_of_integers, 0, len(list_of_integers)-1)
