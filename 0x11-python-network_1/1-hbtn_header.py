b369fcd2adcc71afc8ee1f90e4354a9d5e73ef3e
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-13 23:46:43 +0000

    Using GET method instead

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,13 +4,11 @@ from sys import argv
 from urllib.request import urlopen, Request
 
 if __name__ == "__main__":
+
     req = Request(argv[1])
+
     with urlopen(req) as response:
 
         allinfo = response.info()
 
-        headers = allinfo.__dict__['_headers']
-
-        header_dict = {k: v for k, v in headers}
-
-        print(header_dict['X-Request-Id'])
+        print(allinfo.get('X-Request-Id'))
