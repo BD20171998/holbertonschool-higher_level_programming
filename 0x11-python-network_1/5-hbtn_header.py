5290bc65930ba4197166251bf8cd9ab76f529e1a
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-13 23:53:53 +0000

    Removed un-needed variable

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,6 +7,4 @@ if __name__ == "__main__":
 
     html = requests.get(sys.argv[1])
 
-    html_head_dict = html.headers
-
-    print(html_head_dict['x-request-id'])
+    print(html.headers['x-request-id'])
