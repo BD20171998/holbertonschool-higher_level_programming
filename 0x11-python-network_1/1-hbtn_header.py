dee2b9f95613a59a921dd6040b5e901acf77c3f6
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-13 21:29:51 +0000

    Added Request method

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 from sys import argv
-from urllib.request import urlopen, urlretrieve
+from urllib.request import urlopen, Request
 
 if __name__ == "__main__":
-    with urlopen(argv[1]) as response:
+    req = Request(argv[1])
+    with urlopen(req) as response:
+
         allinfo = response.info()
 
         headers = allinfo.__dict__['_headers']
