87a4fc89b17d89ec274edd108198d03fc3fa5e84
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-12 23:32:37 +0000

    Adding JS file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const args = process.argv;
+const requestURL = args[2];
+
+request.get(requestURL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const list = {};
+    let user = 'default';
+
+    for (let i = 0; i < data.length; i++) {
+      if (list[i] === undefined) {
+        user = data[i].userId;
+        list[user] = 0;
+      }
+    }
+
+    for (let j = 0; j < data.length; j++) {
+      if (data[j].completed === true) {
+        user = data[j].userId;
+        list[user] += 1;
+      }
+    }
+
+    console.log(list);
+  }
+});
