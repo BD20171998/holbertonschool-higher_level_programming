919f14bd8be9d0c88d8b0117621ab5eedefe424e
Bob Deprizio <robert.deprizio@gmail.com>
2020-05-13 04:59:54 +0000

    Removed 2nd for loop

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,22 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const args = process.argv;
 const requestURL = args[2];
-
 request.get(requestURL, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
     const data = JSON.parse(body);
-    if (data.length === undefined) {
-      throw err;
-    }
     const list = {};
     let user = 'default';
-
     for (let i = 0; i < data.length; i++) {
-      if (list[i] === undefined) {
-        user = data[i].userId;
-        list[user] = 0;
-      }
-    }
-
-    for (let j = 0; j < data.length; j++) {
-      if (data[j].completed === true) {
-        user = data[j].userId;
-        list[user] += 1;
+      if (data[i].completed === true) {
+        if (!(data[i].userId in list)) {
+          user = data[i].userId;
+          list[user] = 1;
+        } else {
+          list[user] += 1;
+        }
       }
     }
 
